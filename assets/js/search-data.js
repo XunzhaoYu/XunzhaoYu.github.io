2c3b241a83923d680b6cf2688fbb045be0aef8eb
XunzhaoYu <yuxunzhao@gmail.com>
2025-11-02 03:17:58 +0000

    Deploying to gh-pages from @ XunzhaoYu/XunzhaoYu.github.io@2b1e61f48d59e4ee9f50d68e07954d156de144f3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "Teaching",
-          description: "TA for several courses at School of Computer Science, University of Birmingham.",
+          description: "TA for several courses at the School of Computer Science, University of Birmingham.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
